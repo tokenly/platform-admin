55c62acfcd2987dbcdf7f96afc3ae6f184de8bd7
Devon Weller <devon@tokenly.co>
2017-10-12 08:12:36 -0500

    Compatability update for Laravel >= 5.4

--- src/Controllers/ResourceController.php
+++ src/Controllers/ResourceController.php
@@ -3,7 +3,7 @@
 namespace Tokenly\PlatformAdmin\Controllers;
 
 use Exception;
-use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
--- src/Controllers/XChainBalancesController.php
+++ src/Controllers/XChainBalancesController.php
@@ -2,7 +2,7 @@
 
 namespace Tokenly\PlatformAdmin\Controllers;
 
-use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
